bf99534edb70ab5cd95a8890695525418400759b
andrey <kvakva20022014@gmail.com>
2022-07-07 13:05:05 +0300

    first commit

---
+++ movies/admin.py
@@ -0,0 +1,82 @@
+from ckeditor_uploader import forms
+from django.contrib import admin
+from .models import Category, Genre, Movie, Rating, RatingStar, Review
+from django import forms
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label='Описание',widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)
+
+
+class ReviewInline(admin.TabularInline):
+    """Отзывы на странице фильма"""
+    model = Review
+    extra = 1
+    readonly_fields = ("name", "email")
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    """Фильмы"""
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year")
+    search_fields = ("title", "category__name")
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ("draft",)
+    form = MovieAdminForm
+    fieldsets = (
+        (None, {
+            "fields": ("title", "kinopoisk_id")
+        }),
+        (None, {
+            "fields": ("description", "poster")
+        }),
+        (None, {
+            "fields": (("year", "country"),)
+        }),
+        ("Actors", {
+            "classes": ("collapse",),
+            "fields": (("actors", "directors", "genres", "category"),)
+        }),
+        ("Options", {
+            "fields": (("url", "draft"),)
+        }),
+    )
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Отзывы"""
+    list_display = ("name", "email", "parent", "movie", "id")
+    readonly_fields = ("name", "email")
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Жанры"""
+    list_display = ("name", "url")
+
+
+#@admin.register(Rating)
+#class RatingAdmin(admin.ModelAdmin):
+#    """Рейтинг"""
+#    list_display = ("name", "ip")
+
+
+admin.site.register(RatingStar)
+
+admin.site.site_title = "CinemaZ"
+admin.site.site_header = "CinemaZ"
---
+++ movies/models.py
@@ -0,0 +1,110 @@
+from django.db import models
+from datetime import date
+
+from django.urls import reverse
+
+
+class Category(models.Model):
+    """Категории"""
+    name = models.CharField("Категория", max_length=150)
+    #description = models.TextField("Описание")
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+
+class Genre(models.Model):
+    """Жанры"""
+    name = models.CharField("Имя", max_length=100)
+    #description = models.TextField("Описание")
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+
+class Movie(models.Model):
+    """Фильм"""
+    title = models.CharField("Название", max_length=100)
+    #tagline = models.CharField("Слоган", max_length=100, default='')
+    description = models.TextField("Описание")
+    kinopoisk_id = models.IntegerField("Кинопоиск_id", default=0)
+    poster = models.ImageField("Постер", upload_to="movies/")
+    year = models.PositiveSmallIntegerField("Дата выхода", default=2019)
+    country = models.CharField("Страна", max_length=30)
+    directors = models.CharField("Режиссер", max_length=100)
+    actors = models.CharField("Актеры", max_length=200)
+    genres = models.ManyToManyField(Genre, verbose_name="жанры")
+    #world_premiere = models.DateField("Премьера в мире", default=date.today)
+    category = models.ForeignKey(
+        Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True
+    )
+    url = models.SlugField(max_length=130, unique=True)
+    draft = models.BooleanField("Черновик", default=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"slug": self.url})
+
+    def get_review(self):
+        return self.reviews_set.filter(parent__isnull=True)
+
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
+
+
+class RatingStar(models.Model):
+    """Звезда рейтинга"""
+    value = models.SmallIntegerField("Значение", default=0)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = "Звезда рейтинга"
+        verbose_name_plural = "Звезды рейтинга"
+
+
+class Rating(models.Model):
+    """Рейтинг"""
+    ip = models.CharField("IP адрес", max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="звезда")
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name="фильм")
+
+    def __str__(self):
+        return f"{self.star} - {self.movie}"
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
+
+
+class Review(models.Model):
+    """Отзывы"""
+    email = models.EmailField()
+    name = models.CharField("Имя", max_length=100)
+    text = models.TextField("Сообщение", max_length=5000)
+    parent = models.ForeignKey(
+        'self', verbose_name="Родитель", on_delete=models.SET_NULL, blank=True, null=True
+    )
+    movie = models.ForeignKey(Movie, verbose_name="фильм", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.movie}"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
